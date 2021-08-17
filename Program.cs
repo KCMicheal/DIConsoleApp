e4d8d19db6850b33650d23b6efdec24f77d5e9cb
KCMicheal <kcmicheal@users.noreply.github.com>
2021-08-17 16:00:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DIConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IUserService, UserService>();
+            services.AddSingleton<UserApplication>();
+            var serviceProvider = services.BuildServiceProvider();
+            var userAppService = serviceProvider.GetService<UserApplication>();
+            userAppService.AddUser("John");
+            userAppService.AddUser("Sunny");
+            var allUsers = userAppService.GetUsers();
+            Console.WriteLine($"Registered Users are: {string.Join(", ", allUsers)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIConsoleApp
+{
+        interface IUserService
+        {
+            List<string> GetUsers();
+            void AddUser(string name);
+
+        }
+
+        class UserService: IUserService
+        {
+            private List<string> users = new List<string>();
+            public List<string> GetUsers()
+            {
+                return users;
+            }
+
+            public void AddUser(string name)
+            {
+                users.Add(name);
+            }
+        }
+
+
+        class UserApplication
+        {
+            IUserService _service;
+            public UserApplication(IUserService service)
+            {
+                _service = service;
+            }
+
+            public List<string> GetUsers()
+            {
+                return _service.GetUsers();
+            }
+
+            public void AddUser(string name)
+            {
+                _service.AddUser(name);
+            }
+        }
+}
